c2e9ca115fe04b270b2781e30c46dd38755e558c
Douglas Francis <dougiefrancis@gmail.com>
2021-04-03 09:12:52 +0100

    All 3 widgets now editable

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,35 +8,43 @@ import FinancialData from './FinancialData';
 
 export default function Dashboard( {loggedIn}) {
 
-    const [widgetEditorOpen, setWidgetEditorOpen] = useState(false);
+    const [EditorOpen, setEditorOpen] = useState(false);
+    const [Editor2Open, setEditor2Open] = useState(false);
+    const [Editor3Open, setEditor3Open] = useState(false);
     const [firstWidget, setFirstWidget] = useState("News");
     const [secondWidget, setSecondWidget] = useState("Finance");
     const [thirdWidget, setThirdWidget] = useState("");
 
-    function addNews () {
-        setThirdWidget("News");
-        setWidgetEditorOpen(false);
-    };
-
-    function addFinance () {
-        setThirdWidget("Finance")
-        setWidgetEditorOpen(false);
-    };
 
     function clearWidget (e) {
         e.stopPropagation();
         setThirdWidget("")
-        setWidgetEditorOpen(false);
+        setEditor3Open(false);
     };
       
     return (
         <div className="dashboard">
-            {widgetEditorOpen && (<div id="widget-editor">
+            {EditorOpen && (<div class="widget-editor">
                 <h2>Add Widget</h2>
+                <button onClick={() => { setFirstWidget("News");setEditorOpen(false);}}>News Feed</button>
+                <button onClick={() => {setFirstWidget("Finance")
+        setEditorOpen(false);}}>Company Financial Data</button>
+                <button id="cancel-btn" onClick={()=>setEditorOpen(false)}>Cancel</button>
+                </div>)}
 
-                <button onClick={addNews}>News Feed</button>
-                <button onClick={addFinance}>Company Financial Data</button>
-                <button id="cancel-btn" onClick={()=>setWidgetEditorOpen(false)}>Cancel</button>
+            {Editor2Open && (<div class="widget-editor">
+                <h2>Add Widget</h2>
+                <button onClick={() => { setSecondWidget("News");setEditor2Open(false);}}>News Feed</button>
+                <button onClick={() => {setSecondWidget("Finance")
+        setEditor2Open(false);}}>Company Financial Data</button>
+                <button id="cancel-btn" onClick={()=>setEditor2Open(false)}>Cancel</button>
+                </div>)}
+            {Editor3Open && (<div class="widget-editor">
+                <h2>Add Widget</h2>
+                <button onClick={() => { setThirdWidget("News");setEditor3Open(false);}}>News Feed</button>
+                <button onClick={() => {setThirdWidget("Finance")
+        setEditor3Open(false);}}>Company Financial Data</button>
+                <button id="cancel-btn" onClick={()=>setEditor3Open(false)}>Cancel</button>
                 </div>)}
             
             {loggedIn ? (
@@ -44,7 +52,7 @@ export default function Dashboard( {loggedIn}) {
 
             <div className="dashboard-container" id="news-container">
                 <div>
-                    {firstWidget === "" && <div onClick={()=> setWidgetEditorOpen(true)} className="img-container"><img id='add-widget' src={add}/></div>}
+                    {firstWidget === "" && <div onClick={()=> setEditorOpen(true)} className="img-container"><img id='add-widget' src={add}/></div>}
                     {firstWidget === "News" && <NewsFeed />}
                     {firstWidget === "Finance" && <FinancialData />}
                 </div>
@@ -54,7 +62,7 @@ export default function Dashboard( {loggedIn}) {
 
             <div className="dashboard-container" id="financial-container" >
                 <div>
-                    {secondWidget === "" && <div onClick={()=> setWidgetEditorOpen(true)} className="img-container"><img id='add-widget' src={add}/></div>}
+                    {secondWidget === "" && <div onClick={()=> setEditor2Open(true)} className="img-container"><img id='add-widget' src={add}/></div>}
                     {secondWidget === "News" && <NewsFeed />}
                     {secondWidget === "Finance" && <FinancialData /> }
                 </div>
@@ -64,7 +72,7 @@ export default function Dashboard( {loggedIn}) {
 
             <div  className="dashboard-container" id="api-3">
                 <div>
-                {thirdWidget === "" && <div onClick={()=> setWidgetEditorOpen(true)} className="img-container"><img id='add-widget' src={add}/></div>}
+                {thirdWidget === "" && <div onClick={()=> setEditor3Open(true)} className="img-container"><img id='add-widget' src={add}/></div>}
                 {thirdWidget === "News" && <NewsFeed />}
                 {thirdWidget === "Finance" && <FinancialData />}
 
